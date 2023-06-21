ef0a9540f85c0e1679b66eda121aef855fa1313f
KarimMaged <karim.maged2020@yahoo.com>
2023-06-21 19:39:49 +0300

    Finish the backend part

---
+++ Todo_API/backend/todos/views.py
@@ -0,0 +1,12 @@
+from rest_framework import generics
+from .models import Todos
+from .serializers import TodosSerializer
+
+# Create your views here.
+class ListTodo(generics.ListAPIView):
+    queryset = Todos.objects.all()
+    serializer_class = TodosSerializer
+
+class DetailTodo(generics.RetrieveAPIView):
+    queryset = Todos.objects.all()
+    serializer_class = TodosSerializer
