7779075f9433d06fd6d1d52aeb072f9f963798fe
KarimMaged <karim.maged2020@yahoo.com>
2023-06-21 20:15:55 +0300

    deal with cross origin resource sharing

--- Todo_API/backend/config/settings.py
+++ Todo_API/backend/config/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     # third party
     'rest_framework',
+    'corsheaders',
     # Loacl
     'todos.apps.TodosConfig',
 ]
@@ -57,7 +58,14 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware', # new
 ]
+
+# new
+CORS_ORIGIN_WHITELIST = (
+'http://localhost:3000',
+'http://localhost:8000',
+)
 
 ROOT_URLCONF = 'config.urls'
 
